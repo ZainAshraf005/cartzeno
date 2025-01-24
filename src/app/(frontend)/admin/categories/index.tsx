64a87140bc4b775889038f16253c87c4a8ad6c91
M Zain Ashraf <164740618+ZainAshraf005@users.noreply.github.com>
2025-01-24 11:54:55 +0500

    Delete src/app/(frontend)/admin/categories/index.tsx

--- src/app/(frontend)/admin/categories/index.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-
-// Dynamically import the page component with SSR disabled
-const AdminCategoriesPage = dynamic(() => import("./page"), { ssr: false });
-
-export default AdminCategoriesPage;
